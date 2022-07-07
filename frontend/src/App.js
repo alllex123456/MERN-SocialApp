e721c3846076f17320cbd738abdc3f0cc009261f
alllex123456 <alextanase454@gmail.com>
2022-07-07 18:17:47 +0300

    routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Users } from './users/pages/Users';
+import { NewPlace } from './places/pages/NewPlace';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Users />} />
+        <Route path="places/new" element={<NewPlace />} />
+        <Route path="*" element={<h1>The page was not found</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/places/pages/NewPlace.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const NewPlace = () => {
+  return <div>NewPlace Page</div>;
+};
