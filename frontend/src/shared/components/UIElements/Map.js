2f150e50a3effd272307beeced788fc554d73fc1
alllex123456 <alextanase454@gmail.com>
2022-07-18 20:46:26 +0300

    --

--- frontend/src/shared/components/UIElements/Map.js
+++ frontend/src/shared/components/UIElements/Map.js
@@ -4,7 +4,7 @@ import './Map.css';
 
 export const Map = (props) => {
   const mapRef = useRef();
-  const key = 'AIzaSyA_ueprReUuOO_CzCB2ELbcPgrj0_acq6A';
+  
   const map = new window.google.maps.Map(mapRef.current, {
     zoom: props.zoom,
     center: props.center,
